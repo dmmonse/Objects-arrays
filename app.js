30a9eb3291008ab483cba064d6e426413fb44e68
monse <78182277+dmmonse@users.noreply.github.com>
2022-02-25 17:15:21 -0600

    object and array final touches

--- app.js
+++ app.js
@@ -5,8 +5,23 @@ const course ={
     courseProf: "Macias",
     courseName: "Programming",
     Ssemester: "Spring 2022",
-    contactInfo:["Phone:(210)000-000","Email: abcd@gmail.com"]
+    contactInfo:["Phone:(210)000-000","Email: abcd@gmail.com"] //< array(?)
 
 };
+function thisClass(){
+    alert("Info about this class: "+"         "+ JSON.stringify(course)+
+    "                                             (This is my object attempt on alert...<3");
+   
+};
 
-console.log(course.contactInfo)
+//console.log(course.contactInfo);
+
+//array:
+const careers = ["Web Developer","UX Design","Graphic Designer","Animator","Photographer",
+"Game Developer","Web Content Manager","SEO specialist","App dev","UX researcher"]
+function pickOne() {
+     alert("Your career is: "+ (careers[Math.floor(Math.random() *10)])+ "! :D" );
+     window.location.reload("Refresh");
+  }
+
+  
