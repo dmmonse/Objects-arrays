3b8e9a3fb9b61a37380e00da04e299eaa14af63e
monse <78182277+dmmonse@users.noreply.github.com>
2022-02-25 13:35:45 -0600

    object
    initial info for object

---
+++ app.js
@@ -0,0 +1,12 @@
+//object:
+
+const course ={
+    courseId: "IMED-2351",
+    courseProf: "Macias",
+    courseName: "Programming",
+    Ssemester: "Spring 2022",
+    contactInfo:["Phone:(210)000-000","Email: abcd@gmail.com"]
+
+};
+
+console.log(course.contactInfo)
